3d65cbe5135f7f943738025f4def605d2614cc4e
se-min <pseeq@naver.com>
2021-03-28 11:49:54 +0900

    2021.03.28 - 1st completed

--- ___classExample.php
+++ ___classExample.php
@@ -1,5 +1,5 @@
 <?php
-	include_once $_SERVER["DOCUMENT_ROOT"] ."/classes/myAutoload.php";
+	include_once $_SERVER["DOCUMENT_ROOT"] ."/classes/myHelper.php";
 
 	/* 주의사항 */
 	// run 제외하고 setTable 부터 꼭 해야됨
